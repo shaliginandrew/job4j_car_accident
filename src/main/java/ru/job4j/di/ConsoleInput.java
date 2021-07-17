526d3c0d32345a366e999f5bd81657fff63b5769
shaliginandrew <shaligin.andrew@gmail.com>
2021-07-17 20:10:17 +0300

    Сканирование проекта [#1070]

--- src/main/java/ru/job4j/di/ConsoleInput.java
+++ src/main/java/ru/job4j/di/ConsoleInput.java
@@ -1,7 +1,9 @@
 package ru.job4j.di;
 
-import java.util.Scanner;
+import org.springframework.stereotype.Component;
 
+import java.util.Scanner;
+@Component
 public class ConsoleInput {
     private final Scanner scanner = new Scanner(System.in);
 
